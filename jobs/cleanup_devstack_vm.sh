6e5401a1bb05ca6bc91d0b240e551f1cc78a7888
Victor Laza <vlaza@cloudbasesolutions.com>
2015-08-27 11:33:10 +0300

    Don't delete VM if is debug job

--- jobs/cleanup_devstack_vm.sh
+++ jobs/cleanup_devstack_vm.sh
@@ -4,11 +4,16 @@ source $KEYSTONERC
 
 set +e
 
-echo "Releasing devstack floating IP"
-nova floating-ip-disassociate "$NAME" "$DEVSTACK_FLOATING_IP"
-echo "Removing devstack VM"
-nova delete "$NAME"
-echo "Deleting devstack floating IP"
-nova floating-ip-delete "$DEVSTACK_FLOATING_IP"
+if [ "$IS_DEBUG_JOB" != "yes" ]
+  then
+    echo "Releasing devstack floating IP"
+    nova floating-ip-disassociate "$NAME" "$DEVSTACK_FLOATING_IP"
+    echo "Removing devstack VM"
+    nova delete "$NAME"
+    echo "Deleting devstack floating IP"
+    nova floating-ip-delete "$DEVSTACK_FLOATING_IP"
+  else
+    echo "Not deleting the VMs, cleanup will be done by hand after debug"
+fi
 
 set -e
