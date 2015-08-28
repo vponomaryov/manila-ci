83d3ecefa134efbe79225af1045f03488a5e2f20
petrutlucian94 <petrutlucian94@gmail.com>
2015-08-28 13:58:07 +0300

    Updated manila tempest tests path

--- devstack_vm/bin/run_tests.sh
+++ devstack_vm/bin/run_tests.sh
@@ -17,7 +17,7 @@ TEMPEST_COMMIT="489f5e62"  # 15 June, 2015
 git checkout $TEMPEST_COMMIT
 
 # Install Manila Tempest integration
-cp -r /home/ubuntu/manila/contrib/tempest/tempest/* $TEMPEST_BASE/tempest
+cp -r /opt/stack/manila/contrib/tempest/tempest/* $TEMPEST_BASE/tempest
 
 cd /opt/stack/tempest
 testr list-tests | grep share | grep -Ev "tempest.api.image|tempest.scenario" > "$RUN_TESTS_LIST" || echo "failed to generate list of tests"
