1292ea791e70ee704d87d21c9ca583d88c74870b
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-08-31 14:04:06 +0300

    Wait for instance to become active

--- devstack_vm/bin/create_share_server.sh
+++ devstack_vm/bin/create_share_server.sh
@@ -6,4 +6,5 @@ nova --os-username manila --os-tenant-name service --os-password Passw0rd \
      boot ws2012r2 --image=ws2012r2_kvm \
                    --flavor=100 \
                    --nic net-id=$NET_ID \
-                   --user-data=/home/ubuntu/ssl/winrm_client_cert.pem
+                   --user-data=/home/ubuntu/ssl/winrm_client_cert.pem \
+                   --poll
