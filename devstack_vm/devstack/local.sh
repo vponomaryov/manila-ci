ce7a9968cc2b10ca8b41f95cfdaebd0c7f3c2f53
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-09-01 22:02:52 +0300

    Add debugging details

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -5,15 +5,18 @@ set -e
 source /home/ubuntu/devstack/functions
 source /home/ubuntu/devstack/functions-common
 
+echo "Updating flavors"
 nova flavor-delete 100
 nova flavor-create manila-service-flavor 100 2048 25 2
 
 # Add DNS config to the private network
+echo "Add DNS config to the private network"
 subnet_id=`neutron net-show private | grep subnets | awk '{print $4}'`
 neutron subnet-update $subnet_id --dns_nameservers list=true 8.8.8.8 8.8.4.4
 
 TEMPEST_CONFIG=/opt/stack/tempest/etc/tempest.conf
 
+echo "Updating tempest settings"
 iniset $TEMPEST_CONFIG identity username demo
 iniset $TEMPEST_CONFIG identity password Passw0rd
 iniset $TEMPEST_CONFIG identity tenant_name demo
@@ -39,6 +42,7 @@ iniset $TEMPEST_CONFIG network public_network_id $public_id
 # router_id=`neutron router-list | grep router | awk '{print $2}'
 # iniset $TEMPEST_CONFIG network public_router_id $router_id
 
+echo "Adding the manila image to glance"
 IMAGE_PATH='/home/ubuntu/devstack/files/images/ws2012_r2_kvm_eval.qcow2.gz'
 gunzip -cd $IMAGE_PATH | glance image-create --name "ws2012r2_kvm" \
                                              --container-format bare --disk-format qcow2 \
@@ -47,9 +51,12 @@ gunzip -cd $IMAGE_PATH | glance image-create --name "ws2012r2_kvm" \
 set +e
 
 MANILA_SERVICE_SECGROUP="manila-service"
+echo "Checking security groups"
 
+echo "nova secgroup-list-rules $MANILA_SERVICE_SECGROUP > /dev/null 2>&1 || nova secgroup-create $MANILA_SERVICE_SECGROUP $MANILA_SERVICE_SECGROUP"
 nova secgroup-list-rules $MANILA_SERVICE_SECGROUP > /dev/null 2>&1 || nova secgroup-create $MANILA_SERVICE_SECGROUP $MANILA_SERVICE_SECGROUP
 
+echo "nova secgroup-add-rule $MANILA_SERVICE_SECGROUP tcp 5985 5986 0.0.0.0/0"
 nova secgroup-add-rule $MANILA_SERVICE_SECGROUP tcp 5985 5986 0.0.0.0/0
 
 set -e
