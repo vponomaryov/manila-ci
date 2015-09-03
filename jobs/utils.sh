7243b78d9bcc0c1620647ff4fbcc705b951b7cd5
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-09-03 07:25:26 +0300

    ensure services are configured

--- jobs/utils.sh
+++ jobs/utils.sh
@@ -97,6 +97,7 @@ join_hyperv (){
     run_wsmancmd_with_retry $URL $WIN_USER $WIN_PASS "powershell -ExecutionPolicy RemoteSigned Remove-Item -Recurse -Force c:\Openstack\manila-ci"
     run_wsmancmd_with_retry $URL $WIN_USER $WIN_PASS "git clone -b hyperv-compute https://github.com/cloudbase/manila-ci C:\Openstack\manila-ci"
     set -e
+    run_wsmancmd_with_retry $URL $WIN_USER $WIN_PASS "powershell -ExecutionPolicy RemoteSigned C:\OpenStack\devstack\scripts\EnsureOpenStackServices.ps1 $WIN_USER $WIN_PASS"
     run_wsmancmd_with_retry $URL $WIN_USER $WIN_PASS "powershell -ExecutionPolicy RemoteSigned C:\OpenStack\manila-ci\HyperV\scripts\create-environment.ps1 -devstackIP $FIXED_IP"
 }
 
