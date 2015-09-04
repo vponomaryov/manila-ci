a869cba02c0df6102fcd81324ab581f0337f4176
petrutlucian94 <petrutlucian94@gmail.com>
2015-09-04 20:53:31 +0300

    Our driver merged, no need to cherry-pick

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -84,11 +84,6 @@ cd /opt/stack/manila
 git config --global user.email "microsoft_manila_ci@microsoft.com"
 git config --global user.name "Microsoft Manila CI"
 
-# Apply the patch implementing the Windows SMB driver.
-# TODO: remove this after it merges
-git fetch https://plucian@review.openstack.org/openstack/manila refs/changes/54/200154/24
-cherry_pick FETCH_HEAD
-
 cd /home/ubuntu/devstack
 
 ./unstack.sh
