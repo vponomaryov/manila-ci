2d8eb004fba4b5c4618f7f5bfcea88254f560d1f
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-09-07 17:24:14 +0300

    create VM as normal user

--- devstack_vm/bin/create_share_server.sh
+++ devstack_vm/bin/create_share_server.sh
@@ -4,8 +4,7 @@
 MANILA_SERVICE_SECGROUP="manila-service"
 NET_ID=$(neutron net-list | grep private | awk '{print $2}')
 
-nova --os-username manila --os-tenant-name service --os-password Passw0rd \
-     boot ws2012r2 --image=ws2012r2_kvm \
+nova boot ws2012r2 --image=ws2012r2_kvm \
                    --flavor=100 \
                    --nic net-id=$NET_ID \
                    --user-data=/home/ubuntu/ssl/winrm_client_cert.pem \
