7ae0f1ee5f4949cdc7f96050f040d3a105689b55
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-09-07 14:37:59 +0300

    don't detach hyper-v node if debug

--- jobs/cleanup_devstack_vm.sh
+++ jobs/cleanup_devstack_vm.sh
@@ -8,12 +8,12 @@ source /home/jenkins-slave/tools/keystonerc_admin
 # Loading functions
 source /usr/local/src/manila-ci/jobs/utils.sh
 
-teardown_hyperv $WIN_USER $WIN_PASS $hyperv_node
-
 set +e
 
 if [ "$IS_DEBUG_JOB" != "yes" ]
   then
+    echo "Detaching the hyper-v node"
+    teardown_hyperv $WIN_USER $WIN_PASS $hyperv_node
     echo "Releasing devstack floating IP"
     nova floating-ip-disassociate "$NAME" "$DEVSTACK_FLOATING_IP"
     echo "Removing devstack VM"
