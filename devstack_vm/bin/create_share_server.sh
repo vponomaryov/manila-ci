d1aebb234edd2aa88d496db131b62b11ba535c79
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-09-08 15:26:43 +0300

    Revert "create VM as normal user"
    This reverts commit 2d8eb004fba4b5c4618f7f5bfcea88254f560d1f.

--- devstack_vm/bin/create_share_server.sh
+++ devstack_vm/bin/create_share_server.sh
@@ -4,7 +4,8 @@
 MANILA_SERVICE_SECGROUP="manila-service"
 NET_ID=$(neutron net-list | grep private | awk '{print $2}')
 
-nova boot ws2012r2 --image=ws2012r2_kvm \
+nova --os-username manila --os-tenant-name service --os-password Passw0rd \
+     boot ws2012r2 --image=ws2012r2_kvm \
                    --flavor=100 \
                    --nic net-id=$NET_ID \
                    --user-data=/home/ubuntu/ssl/winrm_client_cert.pem \
