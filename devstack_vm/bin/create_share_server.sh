d7302dbc8190240c8f5c4ac8dafe758e58282dc4
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-09-08 15:38:42 +0300

    Ensure manila service has available secgroup

--- devstack_vm/bin/create_share_server.sh
+++ devstack_vm/bin/create_share_server.sh
@@ -4,6 +4,14 @@
 MANILA_SERVICE_SECGROUP="manila-service"
 NET_ID=$(neutron net-list | grep private | awk '{print $2}')
 
+nova secgroup-delete $MANILA_SERVICE_SECGROUP
+nova --os-username manila --os-tenant-name service --os-password Passw0rd \
+   secgroup-create $MANILA_SERVICE_SECGROUP $MANILA_SERVICE_SECGROUP
+
+echo "Adding security rules to the $MANILA_SERVICE_SECGROUP security group"
+nova secgroup-add-rule $MANILA_SERVICE_SECGROUP tcp 1 65535 0.0.0.0/0
+nova secgroup-add-rule $MANILA_SERVICE_SECGROUP udp 1 65535 0.0.0.0/0
+
 nova --os-username manila --os-tenant-name service --os-password Passw0rd \
      boot ws2012r2 --image=ws2012r2_kvm \
                    --flavor=100 \
