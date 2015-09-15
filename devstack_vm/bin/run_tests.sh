a107d62708bdd5832a216bea345456491a25f7cf
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-09-15 19:28:31 +0300

    fixed test-list generation exit code

--- devstack_vm/bin/run_tests.sh
+++ devstack_vm/bin/run_tests.sh
@@ -20,15 +20,15 @@ export OS_TEST_TIMEOUT=1800
 
 # TODO: run consistency group tests after we adapt our driver to support this feature (should be minimal changes)
 testr list-tests | grep "manila_tempest_tests.tests.api" | grep -v consistency_group | grep -v security_services > "$RUN_TESTS_LIST"
-
-if [[ $? -eq 0 ]]; then
-  testr run --subunit --parallel --load-list=$RUN_TESTS_LIST | subunit-2to1 > /home/ubuntu/tempest/subunit-output.log 2>&1
-  cat /home/ubuntu/tempest/subunit-output.log | /opt/stack/tempest/tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
-  RET=$?
-  cd /home/ubuntu/tempest/
-  python /home/ubuntu/bin/subunit2html.py /home/ubuntu/tempest/subunit-output.log
-  exit $RET
-else
-  echo "failed to generate list of tests"
-  exit 1
+res=$?
+if [ $res -ne 0 ]; then
+    echo "failed to generate list of tests"
+    exit $res
 fi
+
+testr run --subunit --parallel --load-list=$RUN_TESTS_LIST | subunit-2to1 > /home/ubuntu/tempest/subunit-output.log 2>&1
+cat /home/ubuntu/tempest/subunit-output.log | /opt/stack/tempest/tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
+RET=$?
+cd /home/ubuntu/tempest/
+python /home/ubuntu/bin/subunit2html.py /home/ubuntu/tempest/subunit-output.log
+exit $RET
