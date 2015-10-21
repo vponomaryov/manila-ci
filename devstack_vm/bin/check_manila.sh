c27d2275dbe4fdec95172e8686f8b4c397721848
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-10-21 13:36:22 +0300

    Wait for the m-shr service to become available

--- devstack_vm/bin/check_manila.sh
+++ devstack_vm/bin/check_manila.sh
@@ -2,9 +2,31 @@
 
 source /home/ubuntu/keystonerc
 
-MANILA_COUNT=$(manila service-list | grep manila-share | grep -c -w up); 
+MAX_RETRIES=${1:-60}
+RETRY_INTERVAL=${2:-10}
+TIMEOUT=$((MAX_RETRIES * RETRY_INTERVAL))
 
-if [ "$MANILA_COUNT" != 1 ]
-then
-    exit 1
-fi
+TRIES=0
+MANILA_SV_COUNT=0
+
+echo "Ensuring there are manila-share backends available."
+
+while [ $TRIES -lt $MAX_RETRIES -a $MANILA_SV_COUNT -lt 1 ]; do
+    MANILA_SV_COUNT=$(manila service-list | grep manila-share | grep -c -w up)
+    let TRIES=TRIES+1
+
+    if [ "$MANILA_SV_COUNT" -lt 1  ]
+    then
+        echo "Could not find any available m-shr service."
+        if [ $TRIES -eq $MAX_RETRIES ]
+        then
+            echo "Reached the $TIMEOUT seconds timeout."
+            exit 1
+        else
+            echo "Retrying in $RETRY_INTERVAL seconds."
+            echo "Manila services:"
+            manila service-list
+            sleep $RETRY_INTERVAL
+        fi
+    fi
+done
--- jobs/user_share_servers/initialize.sh
+++ jobs/user_share_servers/initialize.sh
@@ -19,3 +19,9 @@ source /usr/local/src/manila-ci/jobs/run_devstack.sh
 # Create the share server used by manila in this scenario
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY  \
     "source /home/ubuntu/keystonerc && /home/ubuntu/bin/create_share_server.sh" 6
+
+# Ensure that the m-shr service is available or wait for it otherwise.
+# Note that for this job type, the service becomes available only after
+# the share server can be reached via WinRM.
+run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY  \
+    "source /home/ubuntu/keystonerc && /home/ubuntu/bin/check_manila.sh" 1
